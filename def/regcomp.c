1c570955731e47139803ff204443171fe1370d0f
Rakuzan05 <75131398+Rakuzan05@users.noreply.github.com>
2021-11-14 13:51:05 +0100

    Correction André

--- def/regcomp.c
+++ def/regcomp.c
@@ -26,7 +26,7 @@ afd regcomp(unilex_t * rpn, uint size) {
             push(&stack, curr_af);
         } else if (rpn[i].val == '[') {
            i += extract_str_in_brackets(&temp, rpn, i);
-           i++; // Pour être au caractère ] (to be at ] char)
+           i++; // Pour être au caractère ]
            afn_chaine(&curr_af, temp);
            push(&stack, curr_af);
            free(temp);
@@ -66,13 +66,11 @@ afd regcomp(unilex_t * rpn, uint size) {
 
 /* Prend un str commençant par un [ et remplis res avec le str
    entre ce premier [ et le prochain ] et retourne la taille de res
-
-   (Take a str starting with a [ and fills res with the str between that
-   first [ and the next ] and returns the size of res) */
+*/
 
 uint extract_str_in_brackets(char** res, unilex_t* arr_ul, uint start) {
     assert(arr_ul[start].val == '[');
-    // calcule la taille du str entre crochets (calculate the size of str in brackets)
+    // calcule la taille du str entre crochets
     uint size = 0;
     uint i = start+1;
     while(arr_ul[i].val != ']') {
